f62a525eda24604075aeef7cb3a95a6b17495076
sainadkarni <snadka2@uic.edu>
2021-12-05 00:02:24 -0600

    configurize sparkapp

--- LogsProcessingSpark/src/main/scala/SparkLogs.scala
+++ LogsProcessingSpark/src/main/scala/SparkLogs.scala
@@ -5,6 +5,7 @@ import org.apache.log4j._
 import java.util.Properties
 import scala.collection.Map
 import Helper._
+import com.typesafe.config.{Config, ConfigFactory}
 import org.apache.kafka.clients.consumer.KafkaConsumer
 
 import java.io.{File, PrintWriter}
@@ -15,59 +16,63 @@ import scala.jdk.CollectionConverters.iterableAsScalaIterableConverter
 
 object SparkLogs {
 
+  val config: Config = ConfigFactory.load("application.conf")
+  val appConfig = config.getConfig("app")
+
   //Generates String report based on spark processing
   def generateReport(count:Map[String, Long], start:Long, end:Long,
-                     first5sample: Array[String], last5sample: Array[String]): String =
+                     firstnsample: Array[String], lastnsample: Array[String]): String =
   {
     val bodyLine1 = "This report is generated for logs between the time frame: "+
       convertMilliToTime(start) + " --- " + convertMilliToTime(end) + "\n"
-    val bodyLine2 = "There was: "  + count.get("ERROR").get + " ERROR messages\n"
-    val bodyLine3 = "There was: "  + count.get("WARN").get + " WARN messages\n\n\n"
-    val bodyLine4 = "First 5 logs in this time period:\n"
-    val bodyLine5 =  "\n\n\n Last 5 Logs in this time period\n"
-    val first5sampleOfLogs = first5sample.mkString("\n")
-    val last5sampleOfLogs = last5sample.mkString("\n")
-    val body = bodyLine1 + bodyLine2 + bodyLine3 + bodyLine4 + first5sampleOfLogs + bodyLine5 + last5sampleOfLogs
+    val bodyLine2 = "There were: "  + count.get("ERROR").get + " ERROR messages\n"
+    val bodyLine3 = "There were: "  + count.get("WARN").get + " WARN messages\n\n\n"
+    val bodyLine4 = s"First ${appConfig.getInt("nSamples")} logs in this time period:\n"
+    val bodyLine5 =  s"\n\n\nLast ${appConfig.getInt("nSamples")} Logs in this time period\n"
+    val firstnsampleOfLogs = firstnsample.mkString("\n")
+    val lastnsampleOfLogs = lastnsample.mkString("\n")
+    val body = bodyLine1 + bodyLine2 + bodyLine3 + bodyLine4 + firstnsampleOfLogs + bodyLine5 + lastnsampleOfLogs
     body
   }
 
   def main(args: Array[String]): Unit =
   {
 
+
     val logger = Logger.getLogger("org")
 
-    //Set up kafka and subscribe to logfilescraper topic
+    //Set up kafka and subscribe to the configured topic
     val props = new Properties()
-    props.put("bootstrap.servers", "localhost:9092")
+    props.put("bootstrap.servers", appConfig.getString("kafka.host"))
     props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
     props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
     props.put("auto.offset.reset", "latest")
     props.put("group.id", "consumer-group")
     val consumer: KafkaConsumer[String, String] = new KafkaConsumer[String, String](props)
-    consumer.subscribe(util.Arrays.asList("logfilescraper"))
+    consumer.subscribe(util.Arrays.asList(appConfig.getString("kafka.topic")))
 
-    logger.info("Subscribed to kafka topic")
+    logger.info(s"Subscribed to kafka topic- ${appConfig.getString("kafka.topic")}")
 
     //get spark context
     val sc = new SparkContext("local[*]", "SparkLogs")
 
-    logger.info("retrieved spark context")
+    logger.info("Retrieved spark context")
 
     //Run kafka listener
     while (true) {
 
-      //check every 2 seconds for new messages from kafka
-      val record = consumer.poll(Duration.ofMillis(2000)).asScala
+      //check every pollingPeriod seconds for new messages from kafka
+      val record = consumer.poll(Duration.ofMillis(appConfig.getLong("kafka.pollingPeriod"))).asScala
 
-      logger.info("polling for kafka messages")
+      logger.info("Polling kafka for messages")
 
-      //when messages received begin processing
+      //Upon receiving messages, begin processing
       if(!record.isEmpty) {
 
-        logger.info("kafka message received")
+        logger.info("A message has been retrieved from kafka")
 
         //write kafka message to file
-        val pw = new PrintWriter(new File("kafkatest.txt" ))
+        val pw = new PrintWriter(new File(appConfig.getString("kafka.outputfile")))
         for (data <- record.iterator) {
           pw.write(data.value())
         }
@@ -75,10 +80,10 @@ object SparkLogs {
 
         logger.info("kafka message written to file")
 
-        logger.info("beginning spark processing")
+        logger.info("Beginning spark processing")
 
         //create rdd from file
-        val lines = sc.textFile("kafkatest.txt")
+        val lines = sc.textFile(appConfig.getString("kafka.outputfile"))
 
         //get number of error and warn messages
         val rddType = lines.map(parseType)
@@ -89,23 +94,23 @@ object SparkLogs {
         val start = rddTime.min()
         val end = rddTime.max()
 
-        //take first 5 logs
-        val first5sample = lines.take(5)
+        //Take first n logs (n configured in config file)
+        val firstnsample = lines.take(appConfig.getInt("nSamples"))
 
-        //get last 5 logs
+        //Get last n logs
         val reversedLastSamples = lines
             .zipWithIndex()
             .map({ case (x, y) => (y, x) })
             .sortByKey(ascending = false)
             .map({ case (x, y) => y })
-            .take(5)
-        val last5Sample = reversedLastSamples.reverse
+            .take(appConfig.getInt("nSamples"))
+        val lastnSample = reversedLastSamples.reverse
 
-        logger.info("spark processing complete")
+        logger.info("Spark processing complete")
 
         //generate report and send email to stakeholder
-        sendEmail(generateReport(countType, start, end, first5sample, last5Sample))
-        logger.info("email sent successfully")
+        sendEmail(generateReport(countType, start, end, firstnsample, lastnSample))
+        logger.info("Email sent successfully")
         pw.write("")
         pw.close
       }
